155025b6ea5a7cd9ff8ba959b3a2158488389cf6
Kevin Pham <phambamkevin@gmail.com>
2020-03-09 11:52:06 -0700

    Set and Confirm Password

---
+++ app/src/main/java/edu/sjsu/cmpe133app/passwordActivity.java
@@ -0,0 +1,19 @@
+package edu.sjsu.cmpe133app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class passwordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password);
+
+
+
+
+    }
+}
