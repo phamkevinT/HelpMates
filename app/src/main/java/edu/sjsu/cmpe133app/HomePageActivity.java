4b39f75208309672c17f48bf59dd437c3fd5bc88
Kevin Pham <phambamkevin@gmail.com>
2020-04-22 11:53:13 -0700

    - Added the toolbar section for hamburger nav-bar - Added some comments for better explanations

--- app/src/main/java/edu/sjsu/cmpe133app/HomePageActivity.java
+++ app/src/main/java/edu/sjsu/cmpe133app/HomePageActivity.java
@@ -1,22 +1,51 @@
 package edu.sjsu.cmpe133app;
 
 import android.os.Bundle;
-
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 
 public class HomePageActivity extends AppCompatActivity {
+    private DrawerLayout drawer;
 
+    /**
+     * Set the layout of the HomePageActivity to have the homepage layout
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.nav_drawer_layout);
-/**
-        Toolbar toolbar;
-        toolbar = findViewById(R.id.toolbar);
+        setContentView(R.layout.homepage);
+
+        // This is the navigation toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-**/
+
+        // This is the hamburger menu
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,
+                drawer,
+                toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    /**
+     * If our navigation bar is opened, pressing the back button on the phone will
+     * close the navigation bar rather than going a page back
+     */
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
 
