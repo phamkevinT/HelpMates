62deb33a6cff9c6dc37db827da5afc351c08cb34
Sharan Patil <sharanpatil1999@gmail.com>
2020-05-07 22:14:51 -0500

    Adding create post activity with added functionality. Posts can now be made offline.

--- app/src/main/java/edu/sjsu/cmpe133app/CreatePostActivity.java
+++ app/src/main/java/edu/sjsu/cmpe133app/CreatePostActivity.java
@@ -2,13 +2,39 @@ package edu.sjsu.cmpe133app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class CreatePostActivity extends AppCompatActivity {
+
+    public static final String CREATE_POST_MESSAGE = "CreatePostActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_post);
+    }
+
+    public void makePost(View view)
+    {
+        RadioButton radioYes = (RadioButton) findViewById(R.id.radioButton2);
+        RadioButton radioNo = (RadioButton) findViewById(R.id.radioButton5);
+        EditText postContent = (EditText) findViewById(R.id.editText3);
+        String postString =  postContent.getText().toString();
+        if (radioYes.isChecked())
+        {
+            Intent openHomePage = new Intent();
+            openHomePage.putExtra(CREATE_POST_MESSAGE, postString);
+            setResult(1, openHomePage);
+            finish();
+        }
+        else if (radioNo.isChecked())
+        {
+
+        }
     }
 }
